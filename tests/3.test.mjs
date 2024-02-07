94640ef1ca6aa8cedfd373d89b256da888385888
citrus327 <citrus327@outlook.com>
2024-02-07 10:42:58 +0800

    feat: 3

--- tests/3.test.mjs
+++ tests/3.test.mjs
@@ -1,38 +1,37 @@
+import { expect } from "vitest";
+import { it } from "vitest";
+
 /**
  * @param {string} s
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  if (s.length <= 1) return s.length;
-  let maxies = [];
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i + 1; j <= s.length; j++) {
-      const slice = s.slice(i, j);
-      if (uniqString(slice)) {
-        maxies.push(slice.length);
-      } else {
-        break;
-      }
+  // 哈希集合，记录每个字符是否出现过
+  const occ = new Set();
+  const n = s.length;
+  // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+  let rk = -1,
+    ans = 0;
+  for (let i = 0; i < n; ++i) {
+    if (i != 0) {
+      // 左指针向右移动一格，移除一个字符
+      occ.delete(s.charAt(i - 1));
     }
+    while (rk + 1 < n && !occ.has(s.charAt(rk + 1))) {
+      // 不断地移动右指针
+      occ.add(s.charAt(rk + 1));
+      ++rk;
+    }
+    // 第 i 到 rk 个字符是一个极长的无重复字符子串
+    ans = Math.max(ans, rk - i + 1);
   }
-  return maxies.sort((a, b) => b - a)[0];
+  return ans;
 };
 
 function uniqString(s) {
   return new Set(s).size === s.length;
 }
 
-import { expect } from "vitest";
-import { it } from "vitest";
-
-it("should pass", () => {
-  expect(lengthOfLongestSubstring("abcabcbb")).toBe(3);
-  expect(lengthOfLongestSubstring("bbbbb")).toBe(1);
-  expect(lengthOfLongestSubstring("pwwkew")).toBe(3);
-  expect(lengthOfLongestSubstring("au")).toBe(2);
-});
-
-it("should check uniq string", () => {
-  expect(uniqString("abc")).toBe(true);
-  expect(uniqString("aac")).toBe(false);
+it("should work", () => {
+  expect(lengthOfLongestSubstring("tmmzuxt")).toBe(5);
 });
